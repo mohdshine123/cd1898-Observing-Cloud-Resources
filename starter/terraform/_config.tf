e957ba928249e9e8f092af8121490f5f93b563cf
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-07-28 00:25:48 +0530

    Update _config.tf

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,12 +1,15 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-terraform-course1"
+     bucket = "udacity-sre-course2-terraform"
      key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
+     region = "us-east-1"
    }
  }
 
  provider "aws" {
+
+  access_key = "AKIA4V246ZITDSQNLHHW"
+  secret_key = "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"
    region = "us-east-2"
 
    default_tags {
